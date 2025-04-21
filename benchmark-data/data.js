2b54d4f17a6ad329d9e0890ae23ca95c3d21ee7c
github-action-benchmark <github@users.noreply.github.com>
2025-04-21 15:11:53 +0000

    add Go Benchmark (go) benchmark result for 102e54258be479f630d0173ef5c259bd45a280be

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745142416033,
+  "lastUpdate": 1745248313164,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -456,6 +456,54 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "974120535 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "admin@srmxy.cn",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "admin@srmxy.cn",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "distinct": true,
+          "id": "102e54258be479f630d0173ef5c259bd45a280be",
+          "message": "refactor",
+          "timestamp": "2025-04-21T23:02:45+08:00",
+          "tree_id": "b6c77fa92afb8c1d9f9108ffd58819f0193e9def",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/102e54258be479f630d0173ef5c259bd45a280be"
+        },
+        "date": 1745248312888,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 36.25,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "994294190 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 36.25,
+            "unit": "ns/op",
+            "extra": "994294190 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "994294190 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "994294190 times\n4 procs"
           }
         ]
       }
