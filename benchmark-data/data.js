23f859c097b68e9fc49f586999d3d6ed72e7c311
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 10:14:22 +0000

    add Go Benchmark (go) benchmark result for 18a251a82313a00c358b8ee6fcff9054b21eef11

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745748113573,
+  "lastUpdate": 1745748862469,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -1872,6 +1872,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "997958934 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45712896+Yeuoly@users.noreply.github.com",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "18a251a82313a00c358b8ee6fcff9054b21eef11",
+          "message": "fix: update moderation model docstring to reflect plugin name correctly (#250)\n\n- Modified the docstring in the moderation model to use the correct format for the plugin name, enhancing clarity and consistency in the documentation.",
+          "timestamp": "2025-04-27T18:11:37+08:00",
+          "tree_id": "28c7e7f0e965508ec573608f81879bd7c09b8f41",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/18a251a82313a00c358b8ee6fcff9054b21eef11"
+        },
+        "date": 1745748862132,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 138327416,
+            "unit": "ns/op\t 1593543 B/op\t   28317 allocs/op",
+            "extra": "248 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 138327416,
+            "unit": "ns/op",
+            "extra": "248 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1593543,
+            "unit": "B/op",
+            "extra": "248 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 28317,
+            "unit": "allocs/op",
+            "extra": "248 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 36.18,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "982733634 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 36.18,
+            "unit": "ns/op",
+            "extra": "982733634 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "982733634 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "982733634 times\n4 procs"
           }
         ]
       }
