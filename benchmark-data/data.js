f7323754ff6efd1db597c70a616c388c5f6c5084
github-action-benchmark <github@users.noreply.github.com>
2025-05-09 04:31:32 +0000

    add Go Benchmark (go) benchmark result for f99e0e8119720eb94972772cc73c6c3ab23236ee

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746765063987,
+  "lastUpdate": 1746765092732,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -2448,6 +2448,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45712896+Yeuoly@users.noreply.github.com",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f99e0e8119720eb94972772cc73c6c3ab23236ee",
+          "message": "feat: add input validation for installed bucket path (#264)\n\n- Implemented validation in NewInstalledBucket to log warnings for non-alphanumeric starting characters and empty paths for installedPath.\n- Updated parameter naming for consistency across methods in InstalledBucket, enhancing code clarity.",
+          "timestamp": "2025-05-09T12:28:47+08:00",
+          "tree_id": "a0b65f858f6e9a6493cbf440b23c1bdc29178c1e",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/f99e0e8119720eb94972772cc73c6c3ab23236ee"
+        },
+        "date": 1746765092334,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 141482491,
+            "unit": "ns/op\t 1593834 B/op\t   28316 allocs/op",
+            "extra": "249 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 141482491,
+            "unit": "ns/op",
+            "extra": "249 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1593834,
+            "unit": "B/op",
+            "extra": "249 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 28316,
+            "unit": "allocs/op",
+            "extra": "249 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 36.16,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "979537180 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 36.16,
+            "unit": "ns/op",
+            "extra": "979537180 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "979537180 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "979537180 times\n4 procs"
           }
         ]
       }
