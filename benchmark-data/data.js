1e10c6fec689e225fc6184386e9cd68d55ec33e3
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 06:25:54 +0000

    add Go Benchmark (go) benchmark result for 6b112bc8b56ffaef3625f1098acaf067204bcc8a

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747721753676,
+  "lastUpdate": 1747722354129,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -3168,6 +3168,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "994082896 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erigocn@gmail.com",
+            "name": "Zhi",
+            "username": "erigo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b112bc8b56ffaef3625f1098acaf067204bcc8a",
+          "message": "feat(redis): Add support for Redis Sentinel mode (#276)\n\n* feat(redis): Add support for Redis Sentinel mode\n\nAdded support for Redis Sentinel mode to the Redis client, enabling automatic discovery and connection to the primary node through Sentinel. Updated relevant configuration files and initialization logic to support Sentinel mode configuration and connection.\n\n* add lost RedisUser.",
+          "timestamp": "2025-05-20T14:23:01+08:00",
+          "tree_id": "52f4f6f60c8a52a509af2265c1d7674bdf0ea07e",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/6b112bc8b56ffaef3625f1098acaf067204bcc8a"
+        },
+        "date": 1747722353606,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 139413313,
+            "unit": "ns/op\t 1593537 B/op\t   28317 allocs/op",
+            "extra": "250 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 139413313,
+            "unit": "ns/op",
+            "extra": "250 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1593537,
+            "unit": "B/op",
+            "extra": "250 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 28317,
+            "unit": "allocs/op",
+            "extra": "250 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 35.95,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "989552910 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 35.95,
+            "unit": "ns/op",
+            "extra": "989552910 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "989552910 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "989552910 times\n4 procs"
           }
         ]
       }
