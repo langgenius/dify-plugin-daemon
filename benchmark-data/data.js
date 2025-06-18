0b52a214169df2ebfc57cadc00f661ea36563f81
github-action-benchmark <github@users.noreply.github.com>
2025-06-18 08:04:02 +0000

    add Go Benchmark (go) benchmark result for a6c8fae9c7a81deeb6e8eff12eedb01383489862

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750219906832,
+  "lastUpdate": 1750233842041,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -5328,6 +5328,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "961885954 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45712896+Yeuoly@users.noreply.github.com",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6c8fae9c7a81deeb6e8eff12eedb01383489862",
+          "message": "feat: add decode plugin from identifier endpoint (#349)\n\n* feat: add decode plugin from identifier endpoint\n\n- Introduced a new endpoint to decode a plugin from a unique identifier.\n- Implemented the DecodePluginFromIdentifier function to handle decoding and verification of plugin signatures.\n- Updated the HTTP server routes to include the new decode endpoint.\n\n* refactor: update decode plugin endpoint path\n\n- Moved the decode plugin from identifier endpoint to a new path for better organization.\n- Updated the HTTP server routes accordingly to reflect the new endpoint structure.",
+          "timestamp": "2025-06-18T16:01:15+08:00",
+          "tree_id": "093c544dd2a09775b758457aaf57db72271d102f",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/a6c8fae9c7a81deeb6e8eff12eedb01383489862"
+        },
+        "date": 1750233841697,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 139248827,
+            "unit": "ns/op\t 1569166 B/op\t   27513 allocs/op",
+            "extra": "252 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 139248827,
+            "unit": "ns/op",
+            "extra": "252 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1569166,
+            "unit": "B/op",
+            "extra": "252 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 27513,
+            "unit": "allocs/op",
+            "extra": "252 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 35.98,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 35.98,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
           }
         ]
       }
