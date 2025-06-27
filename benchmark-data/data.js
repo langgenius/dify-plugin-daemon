cfa5fe0159608c61cde1a6e17b56790496c7ee14
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 11:27:46 +0000

    add Go Benchmark (go) benchmark result for a70d808dd0e8e900d81cb97e0f49254c81711698

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750931323412,
+  "lastUpdate": 1751023666122,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -5976,6 +5976,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "984038859 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45712896+Yeuoly@users.noreply.github.com",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a70d808dd0e8e900d81cb97e0f49254c81711698",
+          "message": "feat(dynamic_select): implement dynamic parameter fetching functionality (#358)\n\n* feat(dynamic_select): implement dynamic parameter fetching functionality\n\n- Added FetchDynamicParameterOptions function to handle dynamic parameter selection.\n- Introduced new access type and action for dynamic select in access_types.\n- Updated HTTP server routes to include the new endpoint for fetching dynamic parameters.\n- Created necessary service and controller files for dynamic select operations.\n\n* refactor(access_types): rename dynamic select access type to dynamic parameter\n\n- Updated access type constants to reflect the change from PLUGIN_ACCESS_TYPE_DYNAMIC_SELECT to PLUGIN_ACCESS_TYPE_DYNAMIC_PARAMETER.\n- Adjusted related references in the PluginDispatchers and FetchDynamicParameterOptions function to maintain consistency.",
+          "timestamp": "2025-06-27T19:24:59+08:00",
+          "tree_id": "1243ad7e37c254e947b195ece9930dd463866a2b",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/a70d808dd0e8e900d81cb97e0f49254c81711698"
+        },
+        "date": 1751023665674,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 141363169,
+            "unit": "ns/op\t 1569560 B/op\t   27514 allocs/op",
+            "extra": "243 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 141363169,
+            "unit": "ns/op",
+            "extra": "243 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1569560,
+            "unit": "B/op",
+            "extra": "243 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 27514,
+            "unit": "allocs/op",
+            "extra": "243 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 36.63,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "984541032 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 36.63,
+            "unit": "ns/op",
+            "extra": "984541032 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "984541032 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "984541032 times\n4 procs"
           }
         ]
       }
