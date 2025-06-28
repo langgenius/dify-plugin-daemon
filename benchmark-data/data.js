6601d67c3b0e6681cbf477effc223122762ecf36
github-action-benchmark <github@users.noreply.github.com>
2025-06-28 04:34:47 +0000

    add Go Benchmark (go) benchmark result for 7a7848b3aea334615514bbaa5f7455c452f73cf1

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751023666122,
+  "lastUpdate": 1751085287576,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -6048,6 +6048,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "984541032 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "63482+defstream@users.noreply.github.com",
+            "name": "@defstream",
+            "username": "defstream"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7a7848b3aea334615514bbaa5f7455c452f73cf1",
+          "message": "Update README.md (#372)",
+          "timestamp": "2025-06-28T12:31:57+08:00",
+          "tree_id": "299f602726cb6bc67b574066fd4d5e6228fa8606",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/7a7848b3aea334615514bbaa5f7455c452f73cf1"
+        },
+        "date": 1751085287144,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 140821066,
+            "unit": "ns/op\t 1569241 B/op\t   27514 allocs/op",
+            "extra": "253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 140821066,
+            "unit": "ns/op",
+            "extra": "253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1569241,
+            "unit": "B/op",
+            "extra": "253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 27514,
+            "unit": "allocs/op",
+            "extra": "253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 36.26,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "977814086 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 36.26,
+            "unit": "ns/op",
+            "extra": "977814086 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "977814086 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "977814086 times\n4 procs"
           }
         ]
       }
