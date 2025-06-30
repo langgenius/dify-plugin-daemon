518fa200ac107a196177d923cc0cb3d19b59e03a
github-action-benchmark <github@users.noreply.github.com>
2025-06-30 04:10:45 +0000

    add Go Benchmark (go) benchmark result for 6d6fb3892e4be0682f65bc4f6bc9247d87a4d3dd

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751085287576,
+  "lastUpdate": 1751256645842,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -6120,6 +6120,78 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "977814086 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45712896+Yeuoly@users.noreply.github.com",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d6fb3892e4be0682f65bc4f6bc9247d87a4d3dd",
+          "message": "feat: add InvokeLLMWithStructuredOutput functionality (#369)\n\n* feat: add InvokeLLMWithStructuredOutput functionality\n\n- Introduced a new method InvokeLLMWithStructuredOutput to the BackwardsInvocation interface for handling structured output requests.\n- Added corresponding request and response types to support structured output.\n- Implemented the method in both RealBackwardsInvocation and MockedDifyInvocation for testing purposes.\n- Updated permission handling and task execution for the new structured output invocation type.\n\nThis enhancement allows for more flexible and detailed responses from the LLM, improving the overall functionality of the invocation system.\n\n* refactor: enhance LLMResultChunkWithStructuredOutput structure\n\n- Updated the LLMResultChunkWithStructuredOutput type to include additional fields: Model, SystemFingerprint, and Delta.\n- Added comments to clarify the reasoning behind the structure and the use of type embedding for JSON marshaling.\n\nThis change improves the clarity and functionality of the LLMResultChunkWithStructuredOutput type, ensuring proper JSON serialization.\n\n* refactor: streamline LLMResultChunk construction in InvokeLLMWithStructuredOutput\n\n- Simplified the construction of LLMResultChunk and LLMResultChunkWithStructuredOutput by removing unnecessary type embedding.\n- Enhanced readability and maintainability of the code while preserving functionality.\n\nThis change contributes to cleaner code and improved clarity in the handling of structured output responses.",
+          "timestamp": "2025-06-30T12:07:46+08:00",
+          "tree_id": "32821ebca5412671e0c60776f0a631b64b9d3bb8",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/6d6fb3892e4be0682f65bc4f6bc9247d87a4d3dd"
+        },
+        "date": 1751256645263,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation",
+            "value": 143013429,
+            "unit": "ns/op\t 1569222 B/op\t   27514 allocs/op",
+            "extra": "246 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - ns/op",
+            "value": 143013429,
+            "unit": "ns/op",
+            "extra": "246 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - B/op",
+            "value": 1569222,
+            "unit": "B/op",
+            "extra": "246 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLocalOpenAILLMInvocation - allocs/op",
+            "value": 27514,
+            "unit": "allocs/op",
+            "extra": "246 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream",
+            "value": 37.11,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "974131809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 37.11,
+            "unit": "ns/op",
+            "extra": "974131809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "974131809 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "974131809 times\n4 procs"
           }
         ]
       }
