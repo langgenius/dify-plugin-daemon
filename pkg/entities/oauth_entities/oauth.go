ecc068c32bedb37f77ac58120a5742f590b97a51
Harry <xh001x@hotmail.com>
2025-07-22 00:51:12 +0800

    feat(oauth): add metadata field to OAuthGetCredentialsResult

--- pkg/entities/oauth_entities/oauth.go
+++ pkg/entities/oauth_entities/oauth.go
@@ -5,6 +5,7 @@ type OAuthGetAuthorizationURLResult struct {
 }
 
 type OAuthGetCredentialsResult struct {
+	Metadata    map[string]any `json:"metadata,omitempty"`
 	Credentials map[string]any `json:"credentials"`
 	ExpiresAt   int64          `json:"expires_at"`
 }
