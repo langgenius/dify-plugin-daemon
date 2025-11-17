9ce7002e29d6096d240e4e53fff1482dee325326
Yeuoly <admin@srmxy.cn>
2025-11-17 18:58:17 +0800

    fix: support get serverless runtime

--- internal/core/control_panel/runtime_export.go
+++ internal/core/control_panel/runtime_export.go
@@ -6,6 +6,9 @@ import (
 
 // GetPluginRuntime returns the plugin runtime for the given plugin unique identifier
 // it automatically detects the runtime type and returns the corresponding runtime
+//
+// NOTE: serverless runtime is not supported in this method
+// it only works for runtime which actually running on this machine
 func (c *ControlPanel) GetPluginRuntime(
 	pluginUniqueIdentifier plugin_entities.PluginUniqueIdentifier,
 ) (plugin_entities.PluginRuntimeSessionIOInterface, error) {
