d4fda4b1074904786f1a8d934caa6b1aee2134e5
Yeuoly <admin@srmxy.cn>
2025-11-17 19:30:58 +0800

    fix: avoid ManifestValidate in first step of debugging handshake

--- internal/core/debugging_runtime/runtime_intialization_handlers.go
+++ internal/core/debugging_runtime/runtime_intialization_handlers.go
@@ -150,11 +150,6 @@ func (d *DifyServer) handleDeclarationRegister(
 		return fmt.Errorf("handshake failed, invalid plugin declaration: %v", err)
 	}
 
-	// check if declaration is valid
-	if err := declaration.ManifestValidate(); err != nil {
-		return fmt.Errorf("invalid manifest detected: %v", err)
-	}
-
 	runtime.Config = declaration
 
 	// registration transferred
