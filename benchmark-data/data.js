44a8c795197fbec8e7a003ee69cc8be1197e993c
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 08:14:39 +0000

    add Go Benchmark (go) benchmark result for e10b48fccd804fe7fd1522fe1cbc9060ea502080

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763977013028,
+  "lastUpdate": 1764663279344,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -9792,6 +9792,54 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "988442043 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hjlarry@163.com",
+            "name": "非法操作",
+            "username": "hjlarry"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e10b48fccd804fe7fd1522fe1cbc9060ea502080",
+          "message": "fix: remote debug not work (#515)",
+          "timestamp": "2025-12-02T16:12:17+08:00",
+          "tree_id": "268ef38113b21a0b90f0c4380a48a6980c554352",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/e10b48fccd804fe7fd1522fe1cbc9060ea502080"
+        },
+        "date": 1764663278341,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 39.68,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "906754461 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 39.68,
+            "unit": "ns/op",
+            "extra": "906754461 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "906754461 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "906754461 times\n4 procs"
           }
         ]
       }
