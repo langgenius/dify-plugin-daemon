8eb611a6b4fbf2ced9a9f4ebd98637be9ec52b0d
github-action-benchmark <github@users.noreply.github.com>
2025-12-16 06:15:42 +0000

    add Go Benchmark (go) benchmark result for 72e9a7bc21acd14f7994f0b496da6454b99ee7fc

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765614397118,
+  "lastUpdate": 1765865742585,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -10464,6 +10464,54 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "981869842 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hjlarry@163.com",
+            "name": "非法操作",
+            "username": "hjlarry"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72e9a7bc21acd14f7994f0b496da6454b99ee7fc",
+          "message": "fix: plugin log not display after refactor (#537)\n\n* fix: plugin log not display after refactor\n\n* Update internal/core/local_runtime/notifier_logger.go\n\nCo-authored-by: gemini-code-assist[bot] <176961590+gemini-code-assist[bot]@users.noreply.github.com>\n\n* feat: add logging support for local runtime instance events\n\n---------\n\nCo-authored-by: gemini-code-assist[bot] <176961590+gemini-code-assist[bot]@users.noreply.github.com>\nCo-authored-by: Yeuoly <admin@srmxy.cn>",
+          "timestamp": "2025-12-16T14:14:19+08:00",
+          "tree_id": "0e15981e67ea9ef4607343e4725d27d2db73e7c4",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/72e9a7bc21acd14f7994f0b496da6454b99ee7fc"
+        },
+        "date": 1765865741998,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 31.17,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 31.17,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
           }
         ]
       }
