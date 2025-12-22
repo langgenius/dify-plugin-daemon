d8fdf3be2d3b6aded21e76d86ee132a4f2f912f1
github-action-benchmark <github@users.noreply.github.com>
2025-12-22 10:43:16 +0000

    add Go Benchmark (go) benchmark result for 1773b1a3400bb299e1ac226948cda3762adfca0a

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766130601481,
+  "lastUpdate": 1766400196344,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -10738,6 +10738,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 31.46,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "songyippee@gmail.com",
+            "name": "Yippee Song",
+            "username": "SongYippee"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1773b1a3400bb299e1ac226948cda3762adfca0a",
+          "message": "fix PluginDaemonInternalServerError: no available node, plugin runtime not found (#549)\n\n* fix PluginDaemonInternalServerError: no available node, plugin runtime not found\n\nFix the bug where after a plugin is successfully upgraded, new requests result in the plugin daemon service responding with \"no available node, plugin runtime not found\".\r\n\r\nThe root cause is that when the plugin is upgraded successfully, the corresponding value in Redis is not updated. Consequently, when a new request arrives, it reads the outdated plugin version from Redis and fails to locate the correct runtime.\n\n* raise error if failed",
+          "timestamp": "2025-12-22T18:40:57+08:00",
+          "tree_id": "8701c311a749d8270d561836fca4ace0527984fd",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/1773b1a3400bb299e1ac226948cda3762adfca0a"
+        },
+        "date": 1766400195962,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 30.52,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 30.52,
             "unit": "ns/op",
             "extra": "1000000000 times\n4 procs"
           },
