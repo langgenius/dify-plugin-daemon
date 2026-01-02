0acbe959f3cafa30eaa4a557662c429d15a28a64
github-action-benchmark <github@users.noreply.github.com>
2026-01-02 10:56:27 +0000

    add Go Benchmark (go) benchmark result for 25f20f86928fa8357ae9ca5a033df52070e9e21c

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767063791608,
+  "lastUpdate": 1767351387488,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -10882,6 +10882,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 30.89,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45712896+Yeuoly@users.noreply.github.com",
+            "name": "Yeuoly",
+            "username": "Yeuoly"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "25f20f86928fa8357ae9ca5a033df52070e9e21c",
+          "message": "fix: update redis dependency to v9.6.3 for security improvements (#554)",
+          "timestamp": "2026-01-02T18:54:01+08:00",
+          "tree_id": "e2861d312bc7ab088ae065b302e9a80f5fa487b7",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/25f20f86928fa8357ae9ca5a033df52070e9e21c"
+        },
+        "date": 1767351386895,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 30.94,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 30.94,
             "unit": "ns/op",
             "extra": "1000000000 times\n4 procs"
           },
