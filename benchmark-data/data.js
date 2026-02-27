dd7b654993299f1efce8fe08d7281b9bc17f45c8
github-action-benchmark <github@users.noreply.github.com>
2026-02-27 08:56:00 +0000

    add Go Benchmark (go) benchmark result for 72288c38e83402fea1bcc2d8cffcb938e39ac52f

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771055733467,
+  "lastUpdate": 1772182560163,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -11520,6 +11520,54 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fatelei@gmail.com",
+            "name": "wangxiaolei",
+            "username": "fatelei"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72288c38e83402fea1bcc2d8cffcb938e39ac52f",
+          "message": "fix: ZIP files always use forward slashes / regardless of the operating system (#616)",
+          "timestamp": "2026-02-27T16:53:07+08:00",
+          "tree_id": "923a7066df8cc5cd303faf3b06f9ae4fce39e4c7",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/72288c38e83402fea1bcc2d8cffcb938e39ac52f"
+        },
+        "date": 1772182559503,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 38.5,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "935354570 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 38.5,
+            "unit": "ns/op",
+            "extra": "935354570 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "935354570 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "935354570 times\n4 procs"
           }
         ]
       }
