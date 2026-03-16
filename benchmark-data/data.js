6c17aedad9f3f751b2359fb69be007e697363c3a
github-action-benchmark <github@users.noreply.github.com>
2026-03-16 00:44:11 +0000

    add Go Benchmark (go) benchmark result for 8788e44141cb871554902b0c09093faa6c90d128

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773384083037,
+  "lastUpdate": 1773621851886,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -12240,6 +12240,54 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "973139985 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "947666385@qq.com",
+            "name": "Weichun.Wang",
+            "username": "weichun-wang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8788e44141cb871554902b0c09093faa6c90d128",
+          "message": "fix: remove internal .env loading from plugin daemon (#608)\n\n* fix: make dotenv loading optional and configurable\n\n* refactor: replace dotenv magic strings with constants\n\n* fix: remove internal dotenv loading\n\n* chore: restore unrelated dependency changes after rebase",
+          "timestamp": "2026-03-16T08:41:21+08:00",
+          "tree_id": "21f010c3f5265f8591b106b11e1909583bb164dd",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/8788e44141cb871554902b0c09093faa6c90d128"
+        },
+        "date": 1773621851382,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 38.71,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "933485011 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 38.71,
+            "unit": "ns/op",
+            "extra": "933485011 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "933485011 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "933485011 times\n4 procs"
           }
         ]
       }
