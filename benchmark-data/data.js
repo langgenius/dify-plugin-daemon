93487abae01339fe9ed40f31b3f9891883c3b828
github-action-benchmark <github@users.noreply.github.com>
2026-03-18 05:06:52 +0000

    add Go Benchmark (go) benchmark result for a82c02fb054ce261f07f9f54a700fd0f1b3e3641

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773714213560,
+  "lastUpdate": 1773810412520,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -12384,6 +12384,54 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "933537410 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fatelei@gmail.com",
+            "name": "wangxiaolei",
+            "username": "fatelei"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a82c02fb054ce261f07f9f54a700fd0f1b3e3641",
+          "message": "fix: fix plugin upgrade use context (#639)",
+          "timestamp": "2026-03-18T13:04:03+08:00",
+          "tree_id": "3895bd31bab4a16f4e99c3b7f2aed9af65659354",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/a82c02fb054ce261f07f9f54a700fd0f1b3e3641"
+        },
+        "date": 1773810412021,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 37.66,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "951440394 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 37.66,
+            "unit": "ns/op",
+            "extra": "951440394 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "951440394 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "951440394 times\n4 procs"
           }
         ]
       }
