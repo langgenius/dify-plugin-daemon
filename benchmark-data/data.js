84796a10a13e4245deeb39b790aea4f266bb7ce2
github-action-benchmark <github@users.noreply.github.com>
2026-03-25 12:59:10 +0000

    add Go Benchmark (go) benchmark result for 9b8a221ec41b7d097e48fdaefa1bf1c14b3a930c

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774443298342,
+  "lastUpdate": 1774443549971,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -12754,6 +12754,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 29.59,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41041296+s4s7@users.noreply.github.com",
+            "name": "Shoyo Ito",
+            "username": "s4s7"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b8a221ec41b7d097e48fdaefa1bf1c14b3a930c",
+          "message": "fix: hardcoded http scheme in Dify-Hook-Url (#656)\n\n* Update Dify-Hook-Url handling to support dynamic scheme when empty\n\n* add HeaderDifyHookURL const\n\n* add Dify-Hook-Url handling tests\n\n* fix error handling",
+          "timestamp": "2026-03-25T20:57:01+08:00",
+          "tree_id": "342c55cb9a66cfae2dbb84aceb996d6a42d7660f",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/9b8a221ec41b7d097e48fdaefa1bf1c14b3a930c"
+        },
+        "date": 1774443548862,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 30.38,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 30.38,
             "unit": "ns/op",
             "extra": "1000000000 times\n4 procs"
           },
