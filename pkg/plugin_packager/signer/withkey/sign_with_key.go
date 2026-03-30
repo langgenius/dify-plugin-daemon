2a722a22ed0719ef7cec86a05c84dafc801d566e
Byron.wang <byron@dify.ai>
2026-03-30 14:36:12 +0800

    refactor: remove unused code and deprecated features (#422)
    * clean up unused code
    
    * fix sign_with_key test

--- pkg/plugin_packager/signer/withkey/sign_with_key.go
+++ pkg/plugin_packager/signer/withkey/sign_with_key.go
@@ -6,7 +6,6 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/base64"
-	"errors"
 	"io"
 	"path"
 	"strconv"
@@ -30,10 +29,6 @@ func SignPluginWithPrivateKey(
 		return nil, err
 	}
 
-	if verification == nil {
-		return nil, errors.New("verification cannot be nil")
-	}
-
 	// create a new zip writer
 	zipBuffer := new(bytes.Buffer)
 	zipWriter := zip.NewWriter(zipBuffer)
@@ -74,116 +69,31 @@ func SignPluginWithPrivateKey(
 		return nil, err
 	}
 
-	// write the verification into data
-	// NOTE: .verification.dify.json is a special file that contains the verification information
-	// and it will be placed at the end of the zip file, checksum is calculated using it also
-	verificationBytes := parser.MarshalJsonBytes(verification)
+	if verification != nil {
+		// write the verification into data
+		// NOTE: .verification.dify.json is a special file that contains the verification information
+		// and it will be placed at the end of the zip file, checksum is calculated using it also
+		verificationBytes := parser.MarshalJsonBytes(verification)
 
-	// write verification into the zip file
-	fileWriter, err := zipWriter.Create(consts.VERIFICATION_FILE)
-	if err != nil {
-		return nil, err
-	}
-
-	if _, err := fileWriter.Write(verificationBytes); err != nil {
-		return nil, err
-	}
-
-	// hash the verification
-	hash := sha256.New()
-	hash.Write(verificationBytes)
-	hashed := hash.Sum(nil)
-
-	// write the hash into data
-	if _, err := data.Write(hashed); err != nil {
-		return nil, err
-	}
-
-	// get current time
-	ct := time.Now().Unix()
-
-	// convert time to bytes
-	timeString := strconv.FormatInt(ct, 10)
-
-	// write the time into data
-	data.Write([]byte(timeString))
-
-	// sign the data
-	signature, err := encryption.RSASign(privateKey, data.Bytes())
-	if err != nil {
-		return nil, err
-	}
-
-	// write the signature into the comment field of the zip file
-	comments := parser.MarshalJson(map[string]any{
-		"signature": base64.StdEncoding.EncodeToString(signature),
-		"time":      ct,
-	})
-
-	// write signature
-	err = zipWriter.SetComment(comments)
-	if err != nil {
-		return nil, err
-	}
-
-	// close the zip writer
-	err = zipWriter.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	return zipBuffer.Bytes(), nil
-}
-
-// Only used for testing
-// WARNING: This function is deprecated, use SignPluginWithPrivateKey instead
-func TraditionalSignPlugin(
-	plugin []byte,
-	privateKey *rsa.PrivateKey,
-) ([]byte, error) {
-	decoder, err := decoder.NewZipPluginDecoder(plugin)
-	if err != nil {
-		return nil, err
-	}
-
-	// create a new zip writer
-	zipBuffer := new(bytes.Buffer)
-	zipWriter := zip.NewWriter(zipBuffer)
-
-	defer zipWriter.Close()
-	// store temporary hash
-	data := new(bytes.Buffer)
-	// read one by one
-	err = decoder.Walk(func(filename, dir string) error {
-		file, err := decoder.ReadFile(path.Join(dir, filename))
+		// write verification into the zip file
+		fileWriter, err := zipWriter.Create(consts.VERIFICATION_FILE)
 		if err != nil {
-			return err
+			return nil, err
 		}
 
-		// calculate sha256 hash of the file
+		if _, err := fileWriter.Write(verificationBytes); err != nil {
+			return nil, err
+		}
+
+		// hash the verification
 		hash := sha256.New()
-		hash.Write(file)
+		hash.Write(verificationBytes)
 		hashed := hash.Sum(nil)
 
 		// write the hash into data
-		data.Write(hashed)
-
-		// create a new file in the zip writer
-		fileWriter, err := zipWriter.Create(path.Join(dir, filename))
-		if err != nil {
-			return err
+		if _, err := data.Write(hashed); err != nil {
+			return nil, err
 		}
-
-		_, err = io.Copy(fileWriter, bytes.NewReader(file))
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
 	}
 
 	// get current time
@@ -221,3 +131,4 @@ func TraditionalSignPlugin(
 
 	return zipBuffer.Bytes(), nil
 }
+
