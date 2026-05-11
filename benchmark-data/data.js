b93adda4ca4151b1eb982f981542243157df0592
github-action-benchmark <github@users.noreply.github.com>
2026-05-11 02:49:29 +0000

    add Go Benchmark (go) benchmark result for 9c500812fb91c55afffbd04d2b36029d15b54e88

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778128547406,
+  "lastUpdate": 1778467769338,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -13330,6 +13330,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 19.36,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fatelei@gmail.com",
+            "name": "wangxiaolei",
+            "username": "fatelei"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c500812fb91c55afffbd04d2b36029d15b54e88",
+          "message": "fix: fix remote install plugin id inconsistent plugin_id format (#731)\n\n* fix: fix remote install plugin id inconsistent plugin_id format\n\n* fix: fix review comment",
+          "timestamp": "2026-05-11T10:47:36+08:00",
+          "tree_id": "45953216603aef00a89275987752d1fe25f023da",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/9c500812fb91c55afffbd04d2b36029d15b54e88"
+        },
+        "date": 1778467769057,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 19.21,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 19.21,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           },
