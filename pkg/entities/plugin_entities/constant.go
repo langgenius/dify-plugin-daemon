d35a277967cc7284dcffd2076aef59d2e947ad73
zyqzyq <zyqzyq08@qq.com>
2026-05-14 09:43:07 +0800

    tool add show_on

--- pkg/entities/plugin_entities/constant.go
+++ pkg/entities/plugin_entities/constant.go
@@ -1,5 +1,11 @@
 package plugin_entities
 
+import (
+	"encoding/json"
+
+	"gopkg.in/yaml.v3"
+)
+
 const (
 	SECRET_INPUT   = "secret-input"
 	TEXT_INPUT     = "text-input"
@@ -22,7 +28,46 @@ const (
 )
 
 type ParameterOption struct {
-	Value string     `json:"value" yaml:"value" validate:"required"`
-	Label I18nObject `json:"label" yaml:"label" validate:"required"`
-	Icon  string     `json:"icon" yaml:"icon" validate:"omitempty"`
+	Value  string                      `json:"value" yaml:"value" validate:"required"`
+	Label  I18nObject                  `json:"label" yaml:"label" validate:"required"`
+	Icon   string                      `json:"icon" yaml:"icon" validate:"omitempty"`
+	ShowOn []ToolParameterShowOnObject `json:"show_on" yaml:"show_on" validate:"omitempty,lte=16,dive"`
+}
+
+func (p *ParameterOption) UnmarshalJSON(data []byte) error {
+	type Alias ParameterOption
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(p),
+	}
+
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+
+	if p.ShowOn == nil {
+		p.ShowOn = []ToolParameterShowOnObject{}
+	}
+
+	return nil
+}
+
+func (p *ParameterOption) UnmarshalYAML(value *yaml.Node) error {
+	type Alias ParameterOption
+	aux := &struct {
+		*Alias `yaml:",inline"`
+	}{
+		Alias: (*Alias)(p),
+	}
+
+	if err := value.Decode(&aux); err != nil {
+		return err
+	}
+
+	if p.ShowOn == nil {
+		p.ShowOn = []ToolParameterShowOnObject{}
+	}
+
+	return nil
 }
