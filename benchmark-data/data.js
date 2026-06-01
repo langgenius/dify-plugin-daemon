24e26651214ca46863b8185601b3e3dde7def8a5
github-action-benchmark <github@users.noreply.github.com>
2026-06-01 06:34:08 +0000

    add Go Benchmark (go) benchmark result for b979ebbecd2b918afbff8038043a35ca668bd20a

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780295353854,
+  "lastUpdate": 1780295648643,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -13762,6 +13762,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 19.5,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "52963600+GareArc@users.noreply.github.com",
+            "name": "Xiyuan Chen",
+            "username": "GareArc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b979ebbecd2b918afbff8038043a35ca668bd20a",
+          "message": "fix: clean up ServerlessRuntime record and cache on plugin uninstall (#749)\n\n* fix: delete ServerlessRuntime record on plugin uninstall when refs reach zero\n\n* refactor: expose ClearServerlessRuntimeCache as public method on PluginManager\n\n* fix: clear serverless runtime cache on full plugin uninstall (ENG-474)\n\n* fix: clean up ServerlessRuntime record and cache on plugin upgrade when refs reach zero",
+          "timestamp": "2026-06-01T14:32:13+08:00",
+          "tree_id": "84c1d82cdaef4c103d2a961ff5d32822af920a1b",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/b979ebbecd2b918afbff8038043a35ca668bd20a"
+        },
+        "date": 1780295648333,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 19.66,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 19.66,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           },
