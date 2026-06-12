ff7067cc01cd2e1ae319318a7c01e8474d7efff9
hjlarry <hjlarry@163.com>
2026-06-12 16:30:27 +0800

    chore: more log about aws

--- internal/core/plugin_manager/serverless_runtime/io.go
+++ internal/core/plugin_manager/serverless_runtime/io.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -16,6 +17,48 @@ import (
 	"github.com/langgenius/dify-plugin-daemon/pkg/entities"
 	"github.com/langgenius/dify-plugin-daemon/pkg/entities/plugin_entities"
 )
+
+const serverlessResponsePreviewLimit = 4 * 1024
+
+type limitedBuffer struct {
+	buf   bytes.Buffer
+	limit int
+}
+
+func (b *limitedBuffer) Write(p []byte) (int, error) {
+	originalLen := len(p)
+	if b.limit <= 0 || b.buf.Len() >= b.limit {
+		return originalLen, nil
+	}
+
+	remaining := b.limit - b.buf.Len()
+	if len(p) > remaining {
+		p = p[:remaining]
+	}
+
+	_, err := b.buf.Write(p)
+	return originalLen, err
+}
+
+func (b *limitedBuffer) String() string {
+	return b.buf.String()
+}
+
+func serverlessResponseHeaders(response *http.Response) map[string]string {
+	headers := map[string]string{}
+	for _, key := range []string{
+		"x-amzn-RequestId",
+		"x-amzn-ErrorType",
+		"x-amz-function-error",
+		"content-type",
+		"content-length",
+	} {
+		if value := response.Header.Get(key); value != "" {
+			headers[key] = value
+		}
+	}
+	return headers
+}
 
 func (r *AWSPluginRuntime) Listen(sessionId string) *entities.Broadcast[plugin_entities.SessionMessage] {
 	l := entities.NewBroadcast[plugin_entities.SessionMessage]()
@@ -49,6 +92,7 @@ func (r *AWSPluginRuntime) Write(sessionId string, action access_types.PluginAcc
 	url += "?action=" + string(action)
 
 	connectTime := 240 * time.Second
+	payloadSizeBytes := len(data)
 
 	// create a new http request
 	ctx, cancel := context.WithTimeout(context.Background(), connectTime)
@@ -61,6 +105,14 @@ func (r *AWSPluginRuntime) Write(sessionId string, action access_types.PluginAcc
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "text/event-stream")
 	req.Header.Set("Dify-Plugin-Session-ID", sessionId)
+
+	log.Info(
+		"sending request to aws lambda, session_id=%s action=%s lambda_url=%s payload_size_bytes=%d",
+		sessionId,
+		action,
+		r.LambdaURL,
+		payloadSizeBytes,
+	)
 
 	routine.Submit(map[string]string{
 		"module":     "serverless_runtime",
@@ -89,8 +141,11 @@ func (r *AWSPluginRuntime) Write(sessionId string, action access_types.PluginAcc
 			return
 		}
 
+		defer response.Body.Close()
+
 		// write to data stream
-		scanner := bufio.NewScanner(response.Body)
+		responsePreview := &limitedBuffer{limit: serverlessResponsePreviewLimit}
+		scanner := bufio.NewScanner(io.TeeReader(response.Body, responsePreview))
 
 		// TODO: set a reasonable buffer size or use a reader, this is a temporary solution
 		scanner.Buffer(make([]byte, 1024), 5*1024*1024)
@@ -142,6 +197,20 @@ func (r *AWSPluginRuntime) Write(sessionId string, action access_types.PluginAcc
 					Message:   fmt.Sprintf("failed to read response body: %v", err),
 				}),
 			})
+		}
+
+		if response.StatusCode < 200 || response.StatusCode >= 300 {
+			log.Warn(
+				"aws lambda returned non-success status, session_id=%s action=%s lambda_url=%s payload_size_bytes=%d status=%s status_code=%d headers=%v response_preview=%q",
+				sessionId,
+				action,
+				r.LambdaURL,
+				payloadSizeBytes,
+				response.Status,
+				response.StatusCode,
+				serverlessResponseHeaders(response),
+				responsePreview.String(),
+			)
 		}
 	})
 }
