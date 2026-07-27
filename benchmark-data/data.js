e3042c9b26ec60541d42dfedda7d972290b9cb14
github-action-benchmark <github@users.noreply.github.com>
2026-07-27 04:01:49 +0000

    add Go Benchmark (go) benchmark result for 9ca80b6dce1499aaf78cec1d5bea988e43ae6b54

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784797981421,
+  "lastUpdate": 1785124909073,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -14578,6 +14578,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 19.82,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hjlarry@163.com",
+            "name": "非法操作",
+            "username": "hjlarry"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9ca80b6dce1499aaf78cec1d5bea988e43ae6b54",
+          "message": "fix: route cloud debug plugins to runtime owner (#779)",
+          "timestamp": "2026-07-27T12:00:00+08:00",
+          "tree_id": "2cf5156997d185177c1c9a7baf2024b6144bc766",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/9ca80b6dce1499aaf78cec1d5bea988e43ae6b54"
+        },
+        "date": 1785124908733,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 19.75,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 19.75,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           },
