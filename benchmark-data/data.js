ed72cae532d03254a4015baccdf9e88bb72c4787
github-action-benchmark <github@users.noreply.github.com>
2026-07-28 02:14:02 +0000

    add Go Benchmark (go) benchmark result for de244f79c033a92bd308a2f34b38efd15ee5911f

--- benchmark-data/data.js
+++ benchmark-data/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785124909073,
+  "lastUpdate": 1785204842543,
   "repoUrl": "https://github.com/langgenius/dify-plugin-daemon",
   "entries": {
     "Go Benchmark": [
@@ -14626,6 +14626,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkStream - ns/op",
             "value": 19.75,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - B/op",
+            "value": 15,
+            "unit": "B/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fatelei@gmail.com",
+            "name": "wangxiaolei",
+            "username": "fatelei"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "de244f79c033a92bd308a2f34b38efd15ee5911f",
+          "message": "feat: serverless transcation add heartbeat (#782)",
+          "timestamp": "2026-07-28T10:05:25+08:00",
+          "tree_id": "2ffa72fd5f08f2c1ba2d66552fb19d04bd3d95b1",
+          "url": "https://github.com/langgenius/dify-plugin-daemon/commit/de244f79c033a92bd308a2f34b38efd15ee5911f"
+        },
+        "date": 1785204842124,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStream",
+            "value": 19.86,
+            "unit": "ns/op\t      15 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkStream - ns/op",
+            "value": 19.86,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           },
